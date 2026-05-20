97d3d30e96642681fdd249dc01747aba87810c2b
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-05-21 00:29:53 +0200

    fix: switch order and add really flag

--- build_files/install_himmelblau.sh
+++ build_files/install_himmelblau.sh
@@ -7,7 +7,6 @@ rpm --import https://packages.himmelblau-idm.org/himmelblau.asc
 dnf config-manager addrepo --from-repofile=https://packages.himmelblau-idm.org/stable/latest/rpm/fedora43/himmelblau.repo
 dnf makecache
 dnf install -y himmelblau pam-himmelblau nss-himmelblau himmelblau-sshd-config himmelblau-qr-greeter himmelblau-sso o365 himmelblau-selinux
-aad-tool configure-pam
 
 
 echo "Configuring Authselect for Himmelblau"
@@ -24,3 +23,6 @@ authselect select custom/himmelblau
 
 echo "Enable Himmelblau services"
 sudo systemctl enable himmelblaud himmelblaud-tasks
+
+echo "Configuring PAM for Himmelblau"
+aad-tool configure-pam

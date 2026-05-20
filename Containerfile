9db17c5cb4b62e6338aea2b8862665c878bb678e
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-05-20 23:31:10 +0200

    feat: try to add himmelblau

--- Containerfile
+++ Containerfile
@@ -29,11 +29,14 @@ FROM ghcr.io/ublue-os/aurora:latest
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
 
+COPY system_files/ /
+
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
-    /ctx/build.sh
+    /ctx/build.sh && \
+    /ctx/install_himmelblau.sh
     
 ### LINTING
 ## Verify final image and contents are correct.
---
+++ build_files/install_himmelblau.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+echo "Installing Himmelblau packages and configuring PAM"
+rpm --import https://packages.himmelblau-idm.org/himmelblau.asc
+dnf config-manager addrepo --from-repofile=https://packages.himmelblau-idm.org/stable/latest/rpm/fedora43/himmelblau.repo
+dnf makecache
+dnf install -y himmelblau pam-himmelblau nss-himmelblau himmelblau-sshd-config himmelblau-qr-greeter himmelblau-sso o365 himmelblau-selinux
+aad-tool configure-pam
+
+
+echo "Configuring Authselect for Himmelblau"
+authselect create-profile himmelblau --base-on current
+authselect select himmelblau
+
+echo "Modify nsswitch.conf to use himmelblau for passwd and group"
+sed -i 's/^passwd:.*/& himmelblau/' /etc/authselect/custom/himmelblau/nsswitch.conf
+sed -i 's/^group:.*/& himmelblau/' /etc/authselect/custom/himmelblau/nsswitch.conf
+
+echo "Enable profiles for himmelblau"
+authselect select custom/himmelblau
+
+
+echo "Enable Himmelblau services"
+sudo systemctl enable himmelblaud himmelblaud-tasks
+sudo systemctl restart himmelblaud himmelblaud-tasks

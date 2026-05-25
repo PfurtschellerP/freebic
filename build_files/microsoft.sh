c3ec8b9af35467f7bfe7b3892f3d4b500bfc892e
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-05-25 23:05:37 +0200

    feat: add vscode

--- build_files/microsoft.sh
+++ build_files/microsoft.sh
@@ -6,6 +6,4 @@ echo "Import Microsoft key and add repositories"
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 echo "Installing Microsoft packages"
-dnf makecache
-rm -rf /opt/microsoft
-dnf install -y code microsoft-edge-stable
+dnf install -y code

75a4468cdbb39c50569dc1f3079f91ca3f17c277
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-05-31 09:51:28 +0200

    chore: format

--- build_files/himmelblau.sh
+++ build_files/himmelblau.sh
@@ -8,11 +8,9 @@ dnf config-manager addrepo --from-repofile=https://packages.himmelblau-idm.org/n
 dnf makecache
 dnf install -y himmelblau pam-himmelblau nss-himmelblau himmelblau-sshd-config himmelblau-qr-greeter himmelblau-sso o365 himmelblau-selinux
 
-
 echo "Configuring Authselect for Himmelblau"
 authselect create-profile himmelblau --base-on local
 authselect select himmelblau
-
 
 echo "Modify nsswitch.conf to use himmelblau for passwd and group"
 sed -i 's/^passwd:.*/& himmelblau/' /etc/authselect/custom/himmelblau/nsswitch.conf
@@ -20,7 +18,6 @@ sed -i 's/^group:.*/& himmelblau/' /etc/authselect/custom/himmelblau/nsswitch.co
 
 echo "Enable profiles for himmelblau"
 authselect select custom/himmelblau
-
 
 echo "Enable Himmelblau services"
 sudo systemctl enable himmelblaud himmelblaud-tasks

d9cc472fd03615cb92b5a8222a81ac9b3c8958cf
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-06-18 22:11:18 +0200

    test

--- build_files/himmelblau.sh
+++ build_files/himmelblau.sh
@@ -4,26 +4,24 @@ set -ouex pipefail
 
 echo "Installing Himmelblau packages and configuring PAM"
 rpm --import https://packages.himmelblau-idm.org/himmelblau.asc
-dnf config-manager addrepo --from-repofile=https://packages.himmelblau-idm.org/stable/latest/rpm/fedora43/himmelblau.repo
+dnf config-manager addrepo --from-repofile=https://packages.himmelblau-idm.org/nightly/latest/rpm/fedora44/himmelblau.repo
 dnf makecache
 dnf install -y himmelblau pam-himmelblau nss-himmelblau himmelblau-sshd-config himmelblau-qr-greeter himmelblau-sso himmelblau-selinux
-
-echo "Configuring Authselect for Himmelblau"
-authselect create-profile himmelblau --base-on local
-authselect select himmelblau
-
-echo "Modify nsswitch.conf to use himmelblau for passwd and group"
-sed -i 's/^passwd:.*/& himmelblau/' /etc/authselect/custom/himmelblau/nsswitch.conf
-sed -i 's/^group:.*/& himmelblau/' /etc/authselect/custom/himmelblau/nsswitch.conf
-
-echo "Enable profiles for himmelblau"
-authselect select custom/himmelblau
 
 echo "Enable Himmelblau services"
 sudo systemctl enable himmelblaud himmelblaud-tasks
 
 echo "Configuring PAM for Himmelblau"
 aad-tool configure-pam --really
+
+echo "check if authselect profile is set to himmelblau already"
+authselect current
+
+# should be done by the aad-tool
+# authselect create-profile himmelblau --base-on local
+# authselect select himmelblau
+echo "Set authselect profile to himmelblau"
+authselect select himmelblau
 
 # https://github.com/himmelblau-idm/himmelblau/issues/1042
 # echo "Switch from plasmalogin to gdm"

47491ec18cec1d64daba6c21127c61c1037d8f13
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-06-23 00:07:37 +0200

    test

--- build_files/himmelblau.sh
+++ build_files/himmelblau.sh
@@ -6,10 +6,10 @@ echo "Installing Himmelblau packages and configuring PAM"
 rpm --import https://packages.himmelblau-idm.org/himmelblau.asc
 dnf config-manager addrepo --from-repofile=https://packages.himmelblau-idm.org/stable/latest/rpm/fedora43/himmelblau.repo
 dnf makecache
-dnf install -y himmelblau pam-himmelblau nss-himmelblau # himmelblau-sso himmelblau-selinux
+dnf install -y himmelblau pam-himmelblau nss-himmelblau himmelblau-sso himmelblau-selinux
 
 echo "Enable Himmelblau services"
-# sudo systemctl enable himmelblaud himmelblaud-tasks
+systemctl enable himmelblaud himmelblaud-tasks
 
 echo "Configuring PAM for Himmelblau"
-# aad-tool configure-pam
+aad-tool configure-pam

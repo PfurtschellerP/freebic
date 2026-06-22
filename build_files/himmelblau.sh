dd1db82eb59df7253ed1cf8c41a34fcc485d4bbc
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-06-22 23:26:59 +0200

    test

--- build_files/himmelblau.sh
+++ build_files/himmelblau.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 echo "Installing Himmelblau packages and configuring PAM"
 rpm --import https://packages.himmelblau-idm.org/himmelblau.asc
 dnf config-manager addrepo --from-repofile=https://packages.himmelblau-idm.org/nightly/latest/rpm/fedora44/himmelblau.repo
-# dnf makecache
+dnf makecache
 dnf install -y himmelblau pam-himmelblau nss-himmelblau # himmelblau-sso himmelblau-selinux
 
 echo "Enable Himmelblau services"

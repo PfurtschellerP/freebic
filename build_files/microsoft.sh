a66c8872a4f1a55b9abbdeb24cd08c8b151bb112
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-07-01 15:51:12 +0200

    clear out everything that's not himmelblau in the latest version

--- build_files/microsoft.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ouex pipefail
-
-echo "Import Microsoft key and add repositories"
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-
-echo "Installing Microsoft packages"
-dnf install -y code

800d7c6d060f8ccf562eb1ab9462ebc2130a85c4
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2026-07-01 16:59:05 +0200

    fix: add tpm2-tss package

--- build_files/himmelblau.sh
+++ build_files/himmelblau.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+echo "Install tpm2-tss as the tss group is a requirement for the himmelblau systemd service"
+dnf install tpm2-tss
+
 echo "Installing Himmelblau packages and configuring PAM"
 dnf makecache
 dnf install -y himmelblau pam-himmelblau nss-himmelblau himmelblau-sso himmelblau-selinux
